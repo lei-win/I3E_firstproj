dd335a5670307b64ce44532dce9a6c63a4556d24
lei-win <s10272765@connect.np.edu.sg>
2026-04-23 14:53:56 +0800

    Update mybabyscript.cs

--- Assets/mybabyscript.cs
+++ Assets/mybabyscript.cs
@@ -5,7 +5,7 @@ public class mybabyscript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Hello World");
+        print("Hello my baby boo World");
         
     }
 
